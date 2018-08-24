78880847c382c35f45cec48316cad2b503adbed0
Joe Noonan <joenoonan27@gmail.com>
2018-08-23 19:47:43 -0500

    update

--- config/routes.rb
+++ config/routes.rb
@@ -12,23 +12,30 @@ Rails.application.routes.draw do
 # resources :users, demigod: 'Achillies', constraints: BlacklistConstraint.new
 
 
-  resources :chat, only: [:index]
-  # resources :users, demigod: 'Achillies', constraints: {remote_ip: /127.0.0.1/}
-  resources :users, demigod: 'Achillies', constraints: lambda { |req| !!(req.remote_ip.match '127.0.0.1') if Rails.env == 'development'}
-  resources :dogs
-  resources :sessions, only: [:create, :destroy]
-  resources :adoption
-
+defaults god: :kathulu do
+  # scope :admin do
+    constraints ({id: /\d+/, remote_ip: /127.0.0.1/ }) do
+      resources :chat, only: [:index]
+      # resources :users, demigod: 'Achillies', constraints: {remote_ip: /127.0.0.1/}
+      # resources :users, demigod: 'Achillies', defaults: {god: 'Kathulu'}, constraints: lambda { |req| !!(req.remote_ip.match '127.0.0.1') if Rails.env == 'development'}
+      resources :users, demigod: 'Achillies', constraints: lambda { |req| !!(req.remote_ip.match '127.0.0.1') if Rails.env == 'development'}
+      resources :dogs
+      resources :sessions, only: [:create, :destroy]
+      resources :adoption
+    end
+  # end
+end
   get '/users/error' => 'users#error'
   # root 'users#index'
   root 'users#new'
 
 
 
+
   get '/stories/:id' => redirect("/users/%{id}", status: 302)
 
 
-get '/bears' => 'users#bears'
+  get '/bears' => 'users#bears'
 
 # created own url helpers
 direct :da_bears do
@@ -36,9 +43,21 @@ direct :da_bears do
 end
 
 direct :puppers do
-  {controller: 'dogs', action: 'index', subdomain: 'www'}
+  { controller: 'dogs', action: 'index', subdomain: 'www'}
 end
 
+
+# German path_names
+# scope(path_names: { new: 'neu', edit: 'bearbeiten' }) do
+#   resources :categories, path: 'kategorien'
+# end
+
+
+
+# This will provide you with URLs such as /bob/articles/1 and will allow you to reference the username part of the path as params[:username] in controllers, helpers and views.
+# scope ':username' do
+#   resources :articles
+# end
 
 
 # get 'photos/:id', to: 'photos#show', defaults: { format: 'jpg' }
@@ -71,8 +90,12 @@ end
 # match 'photos', to: 'photos#show', via: :all
 
 # get '*a/foo/*b', to: 'test#index'
+
+
 get 'photos/:id', to: 'photos#show', constraints: { id: /[A-Z]\d{5}/ }
 
+# resources :videos, param: :identifier
+# makes a route like /videos/:identifier/edit
 
 
   mount ActionCable.server => '/cable'
@@ -81,3 +104,5 @@ get 'photos/:id', to: 'photos#show', constraints: { id: /[A-Z]\d{5}/ }
 end
 
 
+# localhost:3000/rails/info/routes
+#regex for finding routes and
