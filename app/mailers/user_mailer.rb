d45c13594ff959a626971c8df8a8b28b54d4f0b1
Joe Noonan <joenoonan27@gmail.com>
2018-08-24 19:28:37 -0500

    update

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,7 +5,8 @@ class UserMailer < ApplicationMailer
   def welcome_email
     @user = params[:user]
     @url = da_bears_url
-    mail(to: 'jgnoonan27@gmail.com', subject: 'See what the bears are up to.')
+    @recipients = 'jgnoonan27@gmail.com' # or ['jgnoonan27@gmail.com', 'jgnoonan27@gmail.com'] or User.pluck :email
+    mail(to: 'jgnoonan27@gmail.com', subject: 'See what the bears are up to.', cc: '', bcc: '')
   end
 end
 
@@ -17,3 +18,6 @@ end
 #  inline attachments
 # def welcome_email
   # attachments.inline['image_name.jpg'] = File.read('/path/to/image.jpg')
+# end
+# then in view
+# <%= image_tag attachments['image.jpg'].url %>
