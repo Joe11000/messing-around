80cc9c18ad0cde83f1ca434a6c09c1ede9b95f2f
Joe Noonan <joenoonan27@gmail.com>
2018-08-30 14:08:14 -0500

    update

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,6 +8,37 @@ RSpec.describe User, type: :model do
     end
   end
 
+  context 'migration' do
+    t.date "birthday", null: false
+    it {is_expected.to(have_db_column(:birthday) \
+                       .of_type(:date)
+                       .with_options(null: false))}
+
+    t.decimal "cash", precision: 10, scale: 2, default: "0.0"
+    it {is_expected.to have_db_column(:cash).of_type(:decimal).with_options(precision: 10, scale: 2, default: '0.0')}
+
+    t.integer "counter_cache"
+
+    it {is_expected.to have_db_column(:)}
+    it {is_expected.to have_db_column(:)}
+    it {is_expected.to have_db_column(:)}
+    it {is_expected.to have_db_column(:)}
+    it {is_expected.to have_db_column(:)}
+    it {is_expected.to have_db_column(:)}
+    it {is_expected.to have_db_column(:)}
+    it {is_expected.to have_db_column(:)}
+    t.datetime "created_at", default: "2018-08-28 21:44:32", null: false
+    t.datetime "deleted_at"
+    t.integer "dogs_count"
+    t.string "email"
+    t.integer "lock_version"
+    t.string "name"
+    t.string "password_digest"
+    t.integer "sexuality", default: 0
+    t.integer "security_clearance", default: 0
+    t.datetime "updated_at", default: "2018-08-28 21:44:32", null: false
+  end
+
   context 'class' do
   end
 
@@ -119,5 +150,29 @@ RSpec.describe User, type: :model do
 
   # it {is_expected.to validate_confirmation_of(:password)}
 
+
+  # it { is_expected.to define_enum_for(:sexuality).with_values(%w(straight bi gay trans)).backed_by_column_of_type(:integer).with_prefix(:society_says).with_suffix(:but_still_fabulous) }
+
+  # it { belongs_to(:xmen).autosave(true) \
+  #                       .class_name(:) \
+  #                       .conditions(powers: true) \
+  #                       .dependent(:) \
+  #                       .inverse_of(:mutants) \
+  #                       .optional \
+  #                       .order(level: :desc) \
+  #                       .required \
+  #                       .with_primary_key(:) \
+  #                       .with_foreign_key(:) \
+  #                       .touch(true) }
+
+  # it { accepts_nested_attributes_for(:dogs).allow_destroy(true) \
+  #                                          .limit(4) \
+  #                                          .update_only(false) }
+
+
+  # it { is_expected.to define_enum_for(:sexuality).backed_by_column_of_type(:integer) \
+  #                            .with_prefix(:prefix) \
+  #                            .with_suffix(:suffix) \
+  #                            .with_values(%w(straight bi gay trans))}
 
 end
